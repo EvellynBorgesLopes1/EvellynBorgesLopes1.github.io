0dad77cb02dbda676dcd3ec14c7aa5d301363447
EvellynBorgesLopes1 <evellyn.lopes@estudante.ifms.edu.br>
2025-01-29 23:55:04 -0300

    atualiza

--- src/routes/pagina1/+page.svelte
+++ src/routes/pagina1/+page.svelte
@@ -1,1 +1,4 @@
 <h1>animes</h1>
+<a href="pagina1"> pagina1</a>
+<a href="pagina2"> pagina2</a>
+<a href="pagina3"> pagina3</a>
--- src/routes/pagina3/+page.svelte
+++ src/routes/pagina3/+page.svelte
@@ -1,1 +1,4 @@
 <h1>Noveis</h1>
+<a href="pagina1"> pagina1</a>
+<a href="pagina2"> pagina2</a>
+<a href="pagina3"> pagina3</a>
