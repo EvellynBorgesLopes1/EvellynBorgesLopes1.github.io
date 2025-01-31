fb49f349963dc00cbb1ab574ec5d505e14782c7b
EvellynBorgesLopes1 <evellyn.lopes@estudante.ifms.edu.br>
2025-01-31 18:02:05 -0300

    atualiza

--- src/routes/Inicio/+page.svelte
+++ src/routes/Inicio/+page.svelte
@@ -1,6 +1,7 @@
 <div class="texto1">
-<h3>Seja bem-vindo ao nosso espaço dedicado ao fascinante universo dos animes e mangas! Aqui, você vai encontrar um mix perfeito entre críticas, análises, novidades e recomendações sobre o que está bombando e o que merece ser descoberto. Se você é apaixonado por essas formas de arte e entretenimento, ou está começando a se aventurar nesse mundo, este é o lugar ideal para você. Vamos explorar juntos tudo o que essas histórias têm a oferecer: desde personagens cativantes e tramas épicas até os detalhes que fazem toda a diferença. Prepare-se para uma jornada cheia de emoções, ação e muita diversão! </h3>
+<h1>Seja bem-vindo ao nosso espaço dedicado ao fascinante universo dos animes e mangas! Aqui, você vai encontrar um mix perfeito entre críticas, análises, novidades e recomendações sobre o que está bombando e o que merece ser descoberto. Se você é apaixonado por essas formas de arte e entretenimento, ou está começando a se aventurar nesse mundo, este é o lugar ideal para você. Vamos explorar juntos tudo o que essas histórias têm a oferecer: desde personagens cativantes e tramas épicas até os detalhes que fazem toda a diferença. Prepare-se para uma jornada cheia de emoções, ação e muita diversão! </h1>
 </div>
+
 <body>
     <style>
       body {
@@ -8,6 +9,9 @@
       }
       .texto1 {
         color: #ffffff ;
+      justify-content: center;
+      align-items: center;
+      
       }
     </style>
 </body>
